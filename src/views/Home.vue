aed91d2605b9fabc93ed813c39acaeec59d35947
Carlos Aguilera <18aguilerac@gmail.com>
2022-09-03 23:35:29 -0700

    admin site for el andariego init

---
+++ src/views/Home.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="d-flex flex-nowrap">
+    <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px; height: 100vh;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+      <span class="fs-4">El Andariego Office</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="btn nav-item">
+        <a href="#" class="nav-link text-white" >
+          Home
+        </a>
+      </li>
+      <li class="btn nav-item">
+        <a href="#" class="nav-link text-white">
+          Dashboard
+        </a>
+      </li>
+      <li class="btn nav-item">
+        <a href="#" class="nav-link text-white">
+          Orders
+        </a>
+      </li>
+      <li class="btn nav-item">
+        <a href="#" class="nav-link text-white">
+          Products
+        </a>
+      </li>
+      <li class="btn nav-item">
+        <a href="#" class="nav-link text-white">
+          Customers
+        </a>
+      </li>
+    </ul>
+    <hr>
+    <div class="dropdown">
+      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+        <strong>mdo</strong>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+        <li><a class="dropdown-item" href="#">New project...</a></li>
+        <li><a class="dropdown-item" href="#">Settings</a></li>
+        <li><a class="dropdown-item" href="#">Profile</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="#">Sign out</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="ms-5">
+    <p>kdljalkjdf</p>
+    <p>kdljalkjdf</p>
+    <p>kdljalkjdf</p>
+    <p>kdljalkjdf</p>
+    <p>kdljalkjdf</p>
+    <p>kdljalkjdf</p>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home-V',
+  components: {
+  }
+}
+</script>
